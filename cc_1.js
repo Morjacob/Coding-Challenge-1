3ee7f84ca68891008459eea35b8108c6932b4873
Morgan Jacob <jacobmmori@gmail.com>
2024-08-31 13:14:48 -0400

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,10 @@
+// Task 1: Variables and Data Types
+
+let employeeName = "Marie Curie";
+const employeeID = 9;
+var isActive = true;
+
+console.log(employeeName,typeof(employeeName));
+console.log(employeeID, typeof(employeeID));
+console.log(isActive,typeof(isActive));
+
