8c641c409d7205094d2874fcfdaa545163118c83
Morgan Jacob <jacobmmori@gmail.com>
2024-08-31 15:38:50 -0400

    Task 3 - Financial Calculations

--- cc_1.js
+++ cc_1.js
@@ -14,8 +14,29 @@ console.log(isActive,typeof(isActive));
 let products = ["Iphone", "Charger", "Headphones"]
 const productDetails = {
         name: products,
-        price: 500,
+        price: 500.00,
         inStock: 10,
 }
 console.log(products, productDetails);
+
+
+//Task 3: Assignment Operators
+
+let accountBalance = 1050.00;
+
+accountBalance += 100;
+console.log('value of accountBalance is : ' +accountBalance)
+
+accountBalance -= 50;
+console.log ('value of accountBalance is : ' -accountBalance)
+
+accountBalance *= 3;
+console.logconsole.log ('value of accountBalance is : ' *accountBalance)
+
+accountBalance /=4
+console.log ('value of accountBalance is : ' /accountBalance)
+
+accountBalance %= 14;
+console.log ('value of accountBalance is : ' %accountBalance)
+
 
