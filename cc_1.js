63a70f63fbb013e19bb690893af910a7691c6d0c
Morgan Jacob <jacobmmori@gmail.com>
2024-08-31 13:36:33 -0400

    Task 2 - Product Inventory Management

--- cc_1.js
+++ cc_1.js
@@ -8,3 +8,14 @@ console.log(employeeName,typeof(employeeName));
 console.log(employeeID, typeof(employeeID));
 console.log(isActive,typeof(isActive));
 
+
+//Task 2: Compound Data Types
+
+let products = ["Iphone", "Charger", "Headphones"]
+const productDetails = {
+        name: products,
+        price: 500,
+        inStock: 10,
+}
+console.log(products, productDetails);
+
